91dbcd54d47c800470315d692bdef754d33eb55a
N3v3rs4nt0S <playcar46@gmail.com>
2019-04-06 02:33:27 -0300

    Create index.py

---
+++ Aula 01/index.py
@@ -0,0 +1,6 @@
+from libs import amendy # Biblioteca personalizada
+
+import wikipedia # API Wikipedia
+import pyttsx3 # Converter texto em voz
+import pybrackets.pybrackets as pybrackets # Limpar a string wikipedia
+import clipboard # Copiar o texto
