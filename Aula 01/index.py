087e1142dca7b82729199b4d666dbbcad366c34d
N3v3rs4nt0S <playcar46@gmail.com>
2019-04-06 02:35:13 -0300

    Delete index.py

--- Aula 01/index.py
+++
@@ -1,6 +0,0 @@
-from libs import amendy # Biblioteca personalizada
-
-import wikipedia # API Wikipedia
-import pyttsx3 # Converter texto em voz
-import pybrackets.pybrackets as pybrackets # Limpar a string wikipedia
-import clipboard # Copiar o texto
