e66bfaab2a45fa18e72512824f401bec77b9f2e6
N3v3rs4nt0S <playcar46@gmail.com>
2019-04-06 03:17:29 -0300

    Create amendy.py

---
+++ Aulas/Aula_02/libs/amendy.py
@@ -0,0 +1,24 @@
+from os import system, name # Inserir codigo no terminal & verificar o SO
+from docx import Document
+from docx.shared import Inches
+
+def systemDetect(): # Verificar SO
+    if(name == 'nt'):
+        return 'win'
+    else:
+        return 'other'
+
+def clearTerminal(): # Limpar terminal
+    if(systemDetect == 'win'):
+        system('cls')
+    else:
+        system('clear')
+
+def createFile(content, theme): # Cria o arquivo do word
+    doc = Document()
+
+    doc.add_heading(theme)
+    doc.add_paragraph(content)
+
+    saveName = theme.replace(' ', '-')
+    return doc.save(f'./Projects/{saveName}.docx')
